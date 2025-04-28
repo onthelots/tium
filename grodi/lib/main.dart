0558eaf19d172f1e1102c6465059e8fbb6184b1f
iOSDevLime <onthelots@naver.com>
2025-04-28 23:08:39 +0900

    init firebase project

--- grodi/lib/main.dart
+++ grodi/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+
+// 
 void main() {
   runApp(const MyApp());
 }
@@ -16,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );ß
+    );
   }
 }
 
